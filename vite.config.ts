c8a5cac866f558c77bf567800fa3568840e6f249
virgenes <cuevavirgen7@gmail.com>
2026-01-07 22:52:49 -0400

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "https://virgenes.github.io/LaCueva/",
   server: {
     host: "::",
     port: 8080,
